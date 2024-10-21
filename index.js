1b3b19c61a2ed0817ff708f4be7c113a321eb03a
jvchosta <joaovitorcosta.oficial@gmail.com>
2024-10-21 12:18:32 -0300

    Atualizações node.js

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
-// Porta do servidor
 const PORT = process.env.PORT || 3000;
 
-app.get('/api/produtos', async (req, res) => {
-    try {
-        const sku = '30850';  // SKU fixo para teste
-        
-        // Configurar a URL da API VTEX
-        const url = `https://panvelprd.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/${sku}`;
+app.get('/api/produtos/:sku', async (req, res) => {
+    const sku = req.params.sku; // Pega o SKU enviado pelo front-end
+    const vtexUrl = `https://panvelprd.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/${sku}`;
 
-        const response = await axios.get(url, {
+    try {
+        const response = await axios.get(vtexUrl, {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
-                'X-VTEX-API-AppKey': 'vtexappkey-panvelprd-OLDAFN',
-                'X-VTEX-API-AppToken': 'UOFVLDXSQIKCFYVTKNGANQCHIWJLHGWBOPXWGORMXUPEYLSHJPNTPXSIHZNDCTTYOLNFWTALWYJEKBMDYEYXZEUSCHZWEAYQUILSCTOOCWIONMKBRUVESGZOFMQRYZUD'
+                'X-VTEX-API-AppToken': 'UOFVLDXSQIKCFYVTKNGANQCHIWJLHGWBOPXWGORMXUPEYLSHJPNTPXSIHZNDCTTYOLNFWTALWYJEKBMDYEYXZEUSCHZWEAYQUILSCTOOCWIONMKBRUVESGZOFMQRYZUD',
+                'X-VTEX-API-AppKey': 'vtexappkey-panvelprd-OLDAFN'
             }
         });
 
-        // Retornar os dados da API VTEX
-        res.json(response.data);
-
+        res.json(response.data); // Retorna o JSON recebido da VTEX para o front-end
     } catch (error) {
-        console.error('Erro na consulta à API:', error);
-        res.status(500).send('Erro ao consultar a API VTEX');
+        console.error('Erro na requisição para a VTEX:', error);
+        res.status(500).json({ error: 'Erro ao consultar produto na VTEX.' });
     }
 });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor proxy rodando na porta ${PORT}`);
 });
