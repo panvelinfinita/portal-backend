fcf8f9c97351ddd279ffb0f6339a69b1b1d66a20
jvchosta <joaovitorcosta.oficial@gmail.com>
2024-10-24 11:05:07 -0300

    uptade simulation

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 
+// Middleware para permitir JSON no body das requisições
+app.use(express.json());
+
 // Configuração do CORS para permitir todas as origens
 app.use(cors({
-    origin:'*',
+    origin: '*',
     methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-type', 'Authorization'] 
+    allowedHeaders: ['Content-type', 'Authorization']
 }));
 
 // Porta do servidor
@@ -18,7 +21,7 @@ app.get('/teste', (req, res) => {
     res.send('Servidor está funcionando');
 });
 
-// Rota para a API
+// Rota para a API de produtos (SKU Front)
 app.get('/api/produtos', async (req, res) => {
     const sku = req.query.sku;  // SKU recebido pela query string
     const url = `https://panvelprd.vtexcommercestable.com.br/api/catalog_system/pvt/sku/stockkeepingunitbyid/${sku}`;
@@ -40,10 +43,6 @@ app.get('/api/produtos', async (req, res) => {
         console.error('Erro ao consultar a API VTEX:', error);
         res.status(500).send('Erro na consulta');
     }
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor proxy rodando na porta ${PORT}`);
 });
 
 // NOVA ROTA: Rota para consultar preço e estoque
@@ -80,6 +79,7 @@ app.post('/api/preco-estoque', async (req, res) => {
     }
 });
 
+// Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor proxy rodando na porta ${PORT}`);
 });
